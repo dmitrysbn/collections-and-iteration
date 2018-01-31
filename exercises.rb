4af5b59237f06e15cfb0795456cb8ea123b111d3
dmitrysbn <dmitry.sbn@gmail.com>
2018-01-31 15:31:40 -0500

    Exercise 0: add hashes

--- exercises.rb
+++ exercises.rb
@@ -3,3 +3,8 @@ ages = [26, 12, 26, 33]
 coin_flips = [true, false, false, true, true, true]
 artists = ["DJ Sprinkles", "Marcel Dettmann", "Galcher Lustwek"]
 fav_colours_symbols = [:red, :white, :black, :neon_green, :ultraviolet]
+
+words = { "book" => "paper with knowledge", "mouse" => "tool", "mousepad" => "carpet for mouse"}
+movies = { "The Matrix" => 1999, "Lord of the Rings" => 2001, "2001: A Space Odyssey" => 1968}
+cities = { "Mexico City" => 25000000, "Moscow" => 20000000, "Toronto" => 3000000}
+friends = {"Andrey" => 12, "Alex" => 33, "Vlad" => 27}
